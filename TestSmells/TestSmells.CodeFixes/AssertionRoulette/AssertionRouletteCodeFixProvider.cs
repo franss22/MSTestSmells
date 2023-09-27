330d563d46b2127a457b91a1af802060b861d783
franss22 <franortizz2001@gmail.com>
2023-09-27 17:55:51 -0300

    Separate Applied and preview Assertion Roulette

--- TestSmells/TestSmells.CodeFixes/AssertionRoulette/AssertionRouletteCodeFixProvider.cs
+++ TestSmells/TestSmells.CodeFixes/AssertionRoulette/AssertionRouletteCodeFixProvider.cs
@@ -12,15 +12,106 @@ using System.Threading;
 using System.Threading.Tasks;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 using Document = Microsoft.CodeAnalysis.Document;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Language.Intellisense;
+using EnvDTE80;
+using Microsoft.CodeAnalysis.Text;
+using System.Collections.Generic;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace TestSmells.AssertionRoulette
 {
+
+    public class CustomOperation : CodeActionOperation
+    {
+        private readonly Func<CancellationToken, Document, Task> SelectText;
+        private readonly Document ChangedDocument;
+
+        public CustomOperation(Func<CancellationToken, Document, Task> action, Document changedDocument)
+        {
+            this.SelectText = action;
+            ChangedDocument = changedDocument;
+        }
+
+        public override void Apply(Workspace workspace, CancellationToken cancellationToken)
+        {
+            // Execute the custom action
+            SelectText(cancellationToken, ChangedDocument).RunSynchronously();
+        }
+    }
+
+
+    public class CustomCodeAction : CodeAction
+    {
+        private readonly Func<CancellationToken, Task<Document>> CreateChangedDocument;
+        public override string EquivalenceKey { get; }
+        public override string Title { get; }
+        private Func<CancellationToken, Document, Task> SelectText;
+
+
+
+        public CustomCodeAction(string title, Func<CancellationToken, Task<Document>> createChangedDocument, string equivalenceKey, Func<CancellationToken, Document, Task> selectText)
+        {
+            Title = title;
+            CreateChangedDocument = createChangedDocument;
+            EquivalenceKey = equivalenceKey;
+            SelectText = selectText;
+        }
+
+
+        protected override async Task<IEnumerable<CodeActionOperation>> ComputePreviewOperationsAsync(CancellationToken cancellationToken)
+        {
+            var changedDocument = await CreateChangedDocument(cancellationToken).ConfigureAwait(false);
+            if (changedDocument == null)
+                return null;
+
+            return new CodeActionOperation[] { new ApplyChangesOperation(changedDocument.Project.Solution), new CustomOperation(SelectText, changedDocument) };
+        }
+
+        protected override async Task<IEnumerable<CodeActionOperation>> ComputeOperationsAsync(CancellationToken cancellationToken)
+        {
+            var changedDocument = await CreateChangedDocument(cancellationToken);
+            if (changedDocument == null)
+                return null;
+            var root = changedDocument.GetSyntaxRootAsync(cancellationToken);
+
+            await SelectText(cancellationToken, changedDocument);
+
+
+            return new CodeActionOperation[] { new ApplyChangesOperation(changedDocument.Project.Solution) };
+        }
+
+
+
+
+
+    }
+
+
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AssertionRouletteCodeFixProvider)), Shared]
     public class AssertionRouletteCodeFixProvider : CodeFixProvider
     {
 
         [Import]
         internal SVsServiceProvider ServiceProvider { get; set; }
+        public static readonly DTE2 DTE;
+        public static readonly IVsTextManager TextManager;
+
+        static AssertionRouletteCodeFixProvider()
+        {
+            try
+            {
+                DTE = Package.GetGlobalService(typeof(DTE)) as DTE2;
+
+            }
+            catch (System.Exception)
+            {
+                DTE = null;
+            }
+        }
+
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
             get { return ImmutableArray.Create(AssertionRouletteAnalyzer.DiagnosticId); }
@@ -40,34 +131,62 @@ namespace TestSmells.AssertionRoulette
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-
             var methodCall = (InvocationExpressionSyntax)root.FindNode(diagnosticSpan);
 
             // Register a code action that will invoke the fix.
             context.RegisterCodeFix(
-                CodeAction.Create(
+                new CustomCodeAction(
                     title: CodeFixResources.CodeFixTitle,
-                    createChangedDocument: c => AddMessageParameterAsync(context.Document, methodCall, c),
+                    createChangedDocument: (c) => AddMessageParameterAsync(context.Document, methodCall, c),
+                    selectText: (c, doc) => SelectText(doc, c),
+                    
                     equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
                 diagnostic);
         }
 
+
+        private async Task SelectText(Document document, CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var changedMessage = root.DescendantNodesAndSelf()
+            .Where(node => node.GetAnnotations("MessageArgument").Any()).FirstOrDefault();
+
+            Location location = changedMessage.GetLocation();
+
+            // Get the line and column information
+            LinePosition startLinePosition = location.GetLineSpan().StartLinePosition;
+            LinePosition endLinePosition = location.GetLineSpan().EndLinePosition;
+
+            int startLine = startLinePosition.Line + 1; // Lines are zero-based, so we add 1 to get the 1-based line number
+            int startColumn = startLinePosition.Character + 1 + 1; // Characters are zero-based, so we add 1 to get the 1-based column number
+
+            int endLine = endLinePosition.Line + 1;
+            int endColumn = endLinePosition.Character + 1 - 1;
+
+            if (DTE != null)
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+                EnvDTE.TextDocument textDocument = DTE.ActiveDocument.Object("TextDocument") as EnvDTE.TextDocument;
+                TextSelection textSelection = textDocument.Selection as TextSelection;
+
+                textSelection.MoveToLineAndOffset(startLine, startColumn, false);
+                textSelection.MoveToLineAndOffset(endLine, endColumn, true);
+            }
+        }
+
         private async Task<Document> AddMessageParameterAsync(Document document, InvocationExpressionSyntax invocation, CancellationToken cancellationToken)
         {
-            //ThreadHelper.ThrowIfNotOnUIThread();
-            DTE dte = (DTE)ServiceProvider.GetService(typeof(DTE));
-
-            var parameters = invocation.ArgumentList;
-                var message = Argument(
+            var message = Argument(
                     LiteralExpression(
                                 SyntaxKind.StringLiteralExpression,
-                                Literal("message")).WithAdditionalAnnotations(RenameAnnotation.Create()));
+                                Literal("message"))).WithAdditionalAnnotations(new SyntaxAnnotation("MessageArgument"));
 
-            InvocationExpressionSyntax newInvocation = invocation.WithArgumentList(parameters.AddArguments(message));
+            InvocationExpressionSyntax newInvocation = invocation.WithArgumentList(invocation.ArgumentList.AddArguments(message));
 
-                var root = await document.GetSyntaxRootAsync(cancellationToken);
-                var newDocument = document.WithSyntaxRoot(root.ReplaceNode(invocation, newInvocation));
-                return newDocument;
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var newDocument = document.WithSyntaxRoot(root.ReplaceNode(invocation, newInvocation));
+
+            return newDocument;
 
         }
     }
