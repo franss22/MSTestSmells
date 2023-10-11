341b7fdd3cccb8fe5d16333ebbe630501c295b90
franss22 <franortizz2001@gmail.com>
2023-10-11 20:31:15 -0300

    IgnoredTest Analyzer

---
+++ TestSmells/TestSmells.Test/IgnoredTest/IgnoredTestUnitTests.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = TestSmells.Test.CSharpAnalyzerVerifier<TestSmells.DuplicateAssert.DuplicateAssertAnalyzer>;
+using TestReading;
+
+namespace TestSmells.Test.DuplicateAssert
+{
+    [TestClass]
+    public class DuplicateAssertUnitTests
+
+    {
+
+        private readonly ReferenceAssemblies UnitTestingAssembly = TestSmellReferenceAssembly.Assemblies();
+
+        private readonly TestReader testReader = new TestReader("DuplicateAssert", "Corpus");
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task EmptyProgram()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+
+
+        [TestMethod]
+        public async Task SimpleDuplicateAssert()
+        {
+            var testFile = @"SimpleDuplicateAssert.cs";
+            var expected = VerifyCS.Diagnostic()
+                .WithSpan(16, 13, 16, 34)//1st assert
+                .WithSpan(11, 21, 11, 32)//method
+                .WithSpan(16, 13, 16, 34)//1st assert
+                .WithSpan(18, 13, 18, 34)//2nd assert
+                .WithArguments("TestMethod1");
+            await new VerifyCS.Test
+            {
+                TestCode = testReader.ReadTest(testFile),
+                ExpectedDiagnostics = { expected },
+                ReferenceAssemblies = UnitTestingAssembly
+            }.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task CommentDifference()
+        {
+            var testFile = @"CommentDifference.cs";
+            var expected = VerifyCS.Diagnostic()
+                .WithSpan(16, 13, 16, 34)
+                .WithSpan(11, 21, 11, 32)
+                .WithSpan(16, 13, 16, 34)
+                .WithSpan(18, 13, 18, 70)
+                .WithArguments("TestMethod1");
+            await new VerifyCS.Test
+            {
+                TestCode = testReader.ReadTest(testFile),
+                ExpectedDiagnostics = { expected },
+                ReferenceAssemblies = UnitTestingAssembly
+            }.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task DifferentArguments()
+        {
+            var testFile = @"DifferentArguments.cs";
+            await new VerifyCS.Test
+            {
+                TestCode = testReader.ReadTest(testFile),
+                ExpectedDiagnostics = { },
+                ReferenceAssemblies = UnitTestingAssembly
+            }.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task DifferentMethods()
+        {
+            var testFile = @"DifferentMethods.cs";
+            await new VerifyCS.Test
+            {
+                TestCode = testReader.ReadTest(testFile),
+                ExpectedDiagnostics = { },
+                ReferenceAssemblies = UnitTestingAssembly
+            }.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task DoubleDiagnostic()
+        {
+            var testFile = @"DoubleDiagnostic.cs";
+            var expected = VerifyCS.Diagnostic()
+                .WithSpan(16, 13, 16, 34)//1st assert
+                .WithSpan(11, 21, 11, 32)//method
+                .WithSpan(16, 13, 16, 34)//1st assert
+                .WithSpan(18, 13, 18, 34)//2nd assert
+                .WithArguments("TestMethod1");
+            var expected2 = VerifyCS.Diagnostic()
+                .WithSpan(20, 13, 20, 46)
+                .WithSpan(11, 21, 11, 32)
+                .WithSpan(20, 13, 20, 46)
+                .WithSpan(22, 13, 22, 46)
+                .WithArguments("TestMethod1");
+            await new VerifyCS.Test
+            {
+                TestCode = testReader.ReadTest(testFile),
+                ExpectedDiagnostics = { expected, expected2 },
+                ReferenceAssemblies = UnitTestingAssembly
+            }.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task WhitespaceDifference()
+        {
+            var testFile = @"WhitespaceDifference.cs";
+            var expected = VerifyCS.Diagnostic()
+                .WithSpan(16, 13, 16, 44)
+                .WithSpan(11, 21, 11, 32)
+                .WithSpan(16, 13, 16, 44)
+                .WithSpan(18, 16, 18, 43)
+                .WithArguments("TestMethod1");
+            await new VerifyCS.Test
+            {
+                TestCode = testReader.ReadTest(testFile),
+                ExpectedDiagnostics = { expected },
+                ReferenceAssemblies = UnitTestingAssembly
+            }.RunAsync();
+        }
+
+
+
+    }
+}
---
+++ TestSmells/TestSmells/IgnoredTest/IgnoredTestAnalyzer.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection.Metadata;
+
+namespace TestSmells.IgnoredTest
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class IgnoredTestAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "IgnoredTest";
+
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Test Smells";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            // Controls analysis of generated code (ex. EntityFramework Migration) None means generated code is not analyzed
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.EnableConcurrentExecution();
+
+            //Registers callback to start analysis
+            context.RegisterCompilationStartAction(FindTestingClass);
+        }
+
+        private static void FindTestingClass(CompilationStartAnalysisContext context)
+        {
+
+            // Get the attribute object from the compilation
+            var testClassAttr = context.Compilation.GetTypeByMetadataName("Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute");
+            if (testClassAttr is null) { return; }
+            var testMethodAttr = context.Compilation.GetTypeByMetadataName("Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute");
+            if (testMethodAttr is null) { return; }
+
+            var ignoreAttr = context.Compilation.GetTypeByMetadataName("Microsoft.VisualStudio.TestTools.UnitTesting.IgnoreAttribute");
+            if (ignoreAttr is null) { return; }
+
+            // We register a Symbol Start Action to filter all test classes and their test methods
+            context.RegisterSymbolStartAction((ctx) =>
+            {
+                if (!TestUtils.TestMethodInTestClass(ctx, testClassAttr, testMethodAttr)) { return; }
+                if (TestUtils.FindAttributeInSymbol(ignoreAttr, ctx.Symbol))
+                {
+                    ctx.RegisterSyntaxNodeAction(ReportIgnoreDiagnostic(ignoreAttr), SyntaxKind.MethodDeclaration);
+                }
+
+            }
+            , SymbolKind.Method);
+        }
+
+        private static Action<SyntaxNodeAnalysisContext> ReportIgnoreDiagnostic(INamedTypeSymbol ignoreAttr)
+        {
+
+            return (SyntaxNodeAnalysisContext context) =>
+            {
+                var methodSyntax = (MethodDeclarationSyntax)context.Node;
+                var attributeList = methodSyntax.AttributeLists.SelectMany(list => list.Attributes);
+
+                
+                foreach (var attribute in attributeList)
+                {
+                    var attributeSymbol = context.SemanticModel.GetSymbolInfo(attribute).Symbol;
+                    if (attributeSymbol is null) { continue;}
+                    if (SymbolEqualityComparer.Default.Equals(ignoreAttr, attributeSymbol))
+                    {
+                        var diagnostic = Diagnostic.Create(Rule, attribute.GetLocation(), methodSyntax.Identifier.Text);
+                        context.ReportDiagnostic(diagnostic);
+                    }
+                }
+                
+            };
+        }
+    }
+}
